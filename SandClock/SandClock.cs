faa27ee8b6690a9ba2de969a259d56f34414c9fd
Elena Boydeva <ellybryant@gmai.com>
2015-10-18 14:35:58 +0300

    Created console app for printing sand clock

---
+++ SandClock/SandClock.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+    class SandClock
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0}", new string('*', n));
+
+            for (int i = 1; i <= n / 2; i++)
+            {
+                Console.WriteLine("{0}{1}{0}", new string('-', (n - (n-i))), new string('*', (n-(2*i))));
+            }
+
+            for (int i = (n/2)-1; i >= 1; i--)
+            {
+                Console.WriteLine("{0}{1}{0}", new string('-', (n - (n - i))), new string('*', (n - (2 * i))));
+            }
+
+            Console.WriteLine("{0}", new string('*', n));
+        }
+    }
